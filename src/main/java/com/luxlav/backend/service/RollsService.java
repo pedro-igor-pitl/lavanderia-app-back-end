7bc8880fe564d7c825c111a1824fece3eee4a54a
PedroIgor <pedro.igor.pitl@gmail.com>
2026-05-08 18:39:19 -0300

    Fix: Ao Tentar acessar ou editar coleta inativa sistema retorna 403

--- src/main/java/com/luxlav/backend/service/RollsService.java
+++ src/main/java/com/luxlav/backend/service/RollsService.java
@@ -5,7 +5,9 @@ import com.luxlav.backend.model.*;
 import com.luxlav.backend.repository.*;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -47,6 +49,13 @@ public class RollsService {
         RollsModel roll = rollsRepository
                 .findById(dto.getId())
                 .orElseThrow(() -> new RuntimeException("Roll não encontrado"));
+
+        if (!roll.getAtivo()) {
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN,
+                    "Coleta inativa"
+            );
+        }
 
         roll.setCodigoManual(dto.getCodigo_manual());
         roll.setDataColeta(dto.getData_coleta());
@@ -144,6 +153,13 @@ public class RollsService {
         RollsModel roll = rollsRepository
                 .findByIdAndCliente_Id(rollId, clienteId)
                 .orElseThrow(() -> new RuntimeException("Roll não encontrado"));
+
+        if (!roll.getAtivo()) {
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN,
+                    "Coleta inativa"
+            );
+        }
 
         List<RollsItensModel> itens = roll.getItens();
 
